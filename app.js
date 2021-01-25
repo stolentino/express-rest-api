2b57320ca26d1ee34fc52bcb10de5cc2ab135feb
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-24 20:59:35 -0800

    add get quote routes to retrieve all and one

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ app.get('/greetings', (reg, res)=>{
     res.json({greeting: "Hello World!"})
 });
 
-app.get('/quotes', (req, res)=>{
-    res.json({greeting: "Hello World!"});
-});
-
 // Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', (req, res)=>{
+  res.json(data);
+});
 // Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', (req, res)=>{
+  //console.log(req.params.id);
+  const quote = data.quotes.find(quote => quote.id == req.params.id);
+  res.json(quote);
+});
 // Send a POST request to /quotes to  CREATE a new quote 
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
 // Send a DELETE request to /quotes/:id DELETE a quote 
