900b8ed7663a47783adb1db941f37eb5c75b7c38
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-05-02 22:20:49 -0700

    refactor get all quotes route to use data.json and records.js

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/greetings', (reg, res)=>{
+const records = require('./records');
+
+/*app.get('/greetings', (reg, res)=>{
     res.json({greeting: "Hello World!"})
-});
+});*/
 
 // Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', (req, res)=>{
-  res.json(data);
+  //res.json(data);
+  //console.log(data);
+  const quotes = records.getQuotes();
+  res.json(quotes);
 });
 // Send a GET request to /quotes/:id to READ(view) a quote
 app.get('/quotes/:id', (req, res)=>{
   //console.log(req.params.id);
-  const quote = data.quotes.find(quote => quote.id == req.params.id);
-  res.json(quote);
+  //const quote = data.quotes.find(quote => quote.id == req.params.id);
+  //res.json(quote);
+  //console.log(quote);
 });
 // Send a POST request to /quotes to  CREATE a new quote 
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
@@ -22,22 +28,3 @@ app.get('/quotes/:id', (req, res)=>{
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
-const data = {
-    quotes: [
-      {
-        id: 8721,
-        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-        author: "Martin Luther King"
-      },
-      {
-        id: 5779,
-        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-        author: "Yvonne Pierre"
-      },
-      {
-        id: 3406,
-        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-        author: "Barbara DeAngelis"
-      }
-    ]
-  }
